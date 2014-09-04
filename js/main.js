266d1350fbce1900951dbf7223587369168fda5b
html5coder <romaalupka@yandex.ru>
2014-09-04 19:17:59 +0400

    yandex map add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    
+    var yandexMapInit = function() {
+        ymaps.ready(init);
+        var myMap,
+            myPlacemark;
+        function init(){
+            myMap = new ymaps.Map("map", {
+                center: [55.7163911,37.4314895],
+                zoom: 17,
+                controls: []
+            });
+
+            myPlacemark = new ymaps.Placemark([55.7163911,37.4314895], {
+                hintContent: 'Москва, ул. Петра Алексеева,  д.12., стр. 2, оф. 104',
+                balloonContent: 'Москва, ул. Петра Алексеева,  д.12., стр. 2, оф. 104'
+            });
+
+            myMap.geoObjects.add(myPlacemark);
+            myMap.behaviors.disable('scrollZoom');
+        }
+    };
+
+    yandexMapInit();
 });
